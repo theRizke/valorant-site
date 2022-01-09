3a62f62849ccd08a48fc6a4f9c26339bd4e3d95d
theRizke <rizkemail@gmail.com>
2022-01-09 16:07:54 +0100

    new folder structure

--- src/pages/Weapons.js
+++ src/pages/Weapons.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Loader from "react-loader-spinner";
 
-import "../button.css";
+import "../css/button.css";
 import "../App.css";
 import "../css/weapons.css";
 
